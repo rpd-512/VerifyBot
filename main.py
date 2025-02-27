be959deda89818f31e7128e844301e063f7d98a3
Rhiddhi Prasad Das <74843655+rpd-512@users.noreply.github.com>
2025-02-27 14:08:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -211,8 +211,7 @@ async def about(interaction: discord.Interaction):
         "**`/verify`** - Sends the verification link.\n"
         "**`/join [server_id]`** - Adds verified users to another server.\n"
         "**`/list`** - Lists all verified users in the server.\n"
-        "**`/about`** - Shows information about the bot.\n"
-        "**`CREATED BY`** - RPD-512, as a part of a fiverr deal [not yet paid for]"
+        "**`/about`** - Shows information about the bot."
     ), inline=False)
 
     embed.set_footer(text="Made for secure user verification and seamless server joining.")
